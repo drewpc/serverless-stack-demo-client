7b859e45432cfe997312ef844642c81c8f09dbb3
PC Drew <pc@thedrews.net>
2021-08-29 17:33:12 -0600

    chore: remove console statement.

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import "./LoadingIndicator.css";
 
 export default function LoadingIndicator() {
-    console.debug('loading indicator');
     return (<div className="lds-grid">
         <div />
         <div />
