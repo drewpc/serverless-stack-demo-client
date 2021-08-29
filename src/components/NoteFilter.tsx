ab332fb17b3209517980147a51c43fe8def18b86
PC Drew <pc@thedrews.net>
2021-08-29 15:19:46 -0600

    refactor: convert the debounced filter function to use the useEffect hook.

--- src/components/NoteFilter.tsx
+++ src/components/NoteFilter.tsx
@@ -1,33 +1,30 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Form from "react-bootstrap/Form";
 
 export default function NoteFilter(props: {
     filterFunction: (value: string) => void,
 }): JSX.Element {
     const [filter, setFilter] = useState("");
+    const filterTimeout = useRef<ReturnType<typeof setTimeout>>();
 
-    let filterTimeout: ReturnType<typeof setTimeout>;
-    const doFilter = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        const unsafeUserInputValue: string = e.target.value;
-        setFilter(unsafeUserInputValue);
-
-        clearTimeout(filterTimeout);
-        filterTimeout = setTimeout(() => {
+    useEffect(() => {
+        clearTimeout(filterTimeout.current as ReturnType<typeof setTimeout>);
+        filterTimeout.current = setTimeout(() => {
             // TODO: sanitize this string to make it safe.
-            props.filterFunction(unsafeUserInputValue);
+            props.filterFunction(filter);
         }, 500);
-    }
+    }, [filter, props.filterFunction]);
 
     return (
         <div className="NoteFilter">
             <Form>
-                <Form.Group controlId="filter">
+                <Form.Group controlId="note-filter">
                     <Form.Label>Filter notes by: </Form.Label>
                     <Form.Control
                         value={filter}
                         type="text"
                         placeholder="Enter text to filter notes"
-                        onChange={doFilter}
+                        onChange={(e) => setFilter(e.target.value)}
                     />
                 </Form.Group>
             </Form>
